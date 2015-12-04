77beadf9a15530fa57d8e138156001ec6176a508
Adam Crews <adamc@slalom.com>
2015-12-04 13:45:09 -0800

    Fix maybe

--- proxytunnel-tls.rb
+++ proxytunnel-tls.rb
@@ -1,14 +1,11 @@
 class ProxytunnelTls < Formula
   desc "Create TCP tunnels through HTTPS proxies - now supporting tls"
   homepage "http://proxytunnel.sourceforge.net/"
-  url "https://github.com/adamcrews/proxytunnel-tls/archive/1.9.2-tls.tar.gz"
-  version "tls"
-  sha256 "c9a32df74368a8a2c81cbb7b27c5fec1180cc8ef5e8c9cc7758e2c537e6101ca"
+  url "https://github.com/adamcrews/proxytunnel-tls/archive/1.9.2-1.tar.gz"
+  version "1"
+  sha256 "a8ed7f463d5adb17fa1dcd26b2142a3d2b356c4c0b61f0417597976c7feb35a5"
 
   depends_on "openssl"
-
-  # Remove conflicting strlcpy/strlcat declarations
-  patch :DATA
 
   def install
     system "make"
@@ -18,29 +15,3 @@ class ProxytunnelTls < Formula
 end
 
 __END__
-diff --git a/Makefile b/Makefile
-index 8d1300c..2e48b6d 100644
---- a/Makefile
-+++ b/Makefile
-@@ -63,8 +63,6 @@ mandir = $(datadir)/man
- # Remove strlcpy/strlcat on (open)bsd/darwin systems
- OBJ = proxytunnel.o	\
- 	base64.o	\
--	strlcpy.o	\
--	strlcat.o	\
- 	strzcat.o	\
- 	setproctitle.o	\
- 	io.o		\
-diff --git a/proxytunnel.h b/proxytunnel.h
-index b948be0..e63c72a 100644
---- a/proxytunnel.h
-+++ b/proxytunnel.h
-@@ -32,8 +32,6 @@ void closeall();
- void do_daemon();
- void initsetproctitle(int argc, char *argv[]);
- void setproctitle(const char *fmt, ...);
--size_t strlcat(char *dst, const char *src, size_t siz);
--size_t strlcpy(char *dst, const char *src, size_t siz);
- size_t strzcat(char *dst, char *format, ...);
- int main( int argc, char *argv[] );
- char * readpassphrase(const char *, char *, size_t, int);
