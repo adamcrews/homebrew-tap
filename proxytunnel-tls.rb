f5d1eb44e47ae8c408e3d19220ebeb6244e1b1f5
Adam Crews <adamc@slalom.com>
2015-12-04 15:42:59 -0800

    Bump

--- proxytunnel-tls.rb
+++ proxytunnel-tls.rb
@@ -1,14 +1,13 @@
 class ProxytunnelTls < Formula
   desc "Create TCP tunnels through HTTPS proxies - now supporting tls"
   homepage "http://proxytunnel.sourceforge.net/"
-  url "https://github.com/adamcrews/proxytunnel-tls/archive/1.9.2-1.tar.gz"
-  version "1"
-  sha256 "a8ed7f463d5adb17fa1dcd26b2142a3d2b356c4c0b61f0417597976c7feb35a5"
+  url "https://github.com/adamcrews/proxytunnel-tls/archive/1.9.2-2.tar.gz"
+  version "2"
+  sha256 "ad4846ef894669482dd297ffc1f3605c356a8bcb6c80333fc75bf9a97ffb004b"
 
   depends_on "openssl"
   depends_on "asciidoc"
   depends_on "xmlto"
-  depends_on "docbook-xsl"
 
   def install
     system "make"
